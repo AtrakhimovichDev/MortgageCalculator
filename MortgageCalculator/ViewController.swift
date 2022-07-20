acc3f88bd58e1c69c4ca30b08f4b8df800a39a8b
Andrei Atrakhimovich <atrakhimovich95@gmail.com>
2022-07-20 15:39:37 +0300

    Initial Commit

---
+++ MortgageCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MortgageCalculator
+//
+//  Created by Andrei Atrakhimovich on 20.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
